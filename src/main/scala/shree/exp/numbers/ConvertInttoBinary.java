95ac45535a0ecdf19d7be0315aeffb1069e0c894
skc382 <skc382@nyu.edu>
2018-01-19 11:09:58 -0600

    add kadanes algo

---
+++ src/main/scala/shree/exp/numbers/ConvertInttoBinary.java
@@ -0,0 +1,7 @@
+package shree.exp.numbers;
+
+/**
+ * Created by schampakaram on 1/18/18.
+ */
+public class ConvertInttoBinary {
+}
---
+++ src/main/scala/shree/exp/numbers/PrintAllSumPairs.java
@@ -0,0 +1,45 @@
+package shree.exp.Arrays;
+
+import java.util.HashMap;
+
+/**
+ * Created by schampakaram on 1/18/18.
+ */
+public class PrintAllSumPairs {
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,6,5,5,10};
+
+        printSum(arr, 10);
+    }
+
+    private static void printSum(int[] arr, int sum) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<arr.length; i++) {
+            int key = arr[i];
+            Integer count;
+            if((count = map.get(key)) == null) {
+                map.put(key, 1);
+            } else {
+                count = count + 1;
+                map.put(key, count);
+            }
+        }
+
+        System.out.println("Pairs:-");
+        for(int i=0; i<arr.length && arr[i] < sum; i++) {
+            int remainder = sum - arr[i];
+
+            Integer count;
+            if((count = map.get(remainder)) != null) {
+                while(count != 0) {
+                    System.out.println(String.format("(%d,%d)", arr[i], remainder));
+                    count--;
+                }
+            }
+        }
+
+    }
+}
